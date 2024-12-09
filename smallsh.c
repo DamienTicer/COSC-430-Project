e54cf3c5aa6d70fab46134dba035b2303d57ff10
Damien Ticer <125095430+DamienTicer@users.noreply.github.com>
2024-12-08 22:24:00 -0500

    Update smallsh.c
    Full functionality of the project specifications

--- smallsh.c
+++ smallsh.c
@@ -3,6 +3,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <string.h>
+#include <signal.h>
+
+//Global variables
+static Job jobs[MAXJOBS];
+static int job_count = 0;
 
 // Function declarations
 int userin(char *p);
@@ -10,15 +16,91 @@ int gettok(char **outptr);
 int inarg(char c);
 int procline(void);
 int runcommand(char **cline, int where);
+void add_job(pid_t pgid, char *command, int status);
+void delete_job(pid_t pgid);
+Job *find_job_by_id(int id);
+void list_jobs();
+void change_job_status(pid_t pgid, int status);
+
 
 /* program buffers and work pointers */
 static char inpbuf[MAXBUF], tokbuf[2*MAXBUF], *ptr = inpbuf, *tok = tokbuf;
+
+/* Global flag for foreground process ID */
+static pid_t foreground_pid = -1;
+
+/* Signal handler for SIGINT */
+void handle_sigtstp(int signo) {
+    if (foreground_pid > 0) {
+        kill(-foreground_pid, SIGTSTP); // Stop the foreground process group
+        change_job_status(foreground_pid, STOPPED);
+        printf("\n");
+        fflush(stdout);
+    }
+}
+
+//Job Handlers
+/* Add a job to the jobs array */
+void add_job(pid_t pgid, char *command, int status) {
+    if (job_count >= MAXJOBS) {
+        printf("Job table full, cannot add job.\n");
+        return;
+    }
+    jobs[job_count].id = job_count + 1;
+    jobs[job_count].pgid = pgid;
+    strncpy(jobs[job_count].command, command, MAXBUF - 1);
+    jobs[job_count].command[MAXBUF - 1] = '\0';
+    jobs[job_count].status = status;
+    job_count++;
+}
+
+/* Delete a job from the jobs array */
+void delete_job(pid_t pgid) {
+    for (int i = 0; i < job_count; i++) {
+        if (jobs[i].pgid == pgid) {
+            for (int j = i; j < job_count - 1; j++) {
+                jobs[j] = jobs[j + 1];
+            }
+            job_count--;
+            return;
+        }
+    }
+}
+
+/* Find a job by its ID */
+Job *find_job_by_id(int id) {
+    for (int i = 0; i < job_count; i++) {
+        if (jobs[i].id == id) {
+            return &jobs[i];
+        }
+    }
+    return NULL;
+}
+
+/* Change the status of a job */
+void change_job_status(pid_t pgid, int status) {
+    for (int i = 0; i < job_count; i++) {
+        if (jobs[i].pgid == pgid) {
+            jobs[i].status = status;
+            return;
+        }
+    }
+}
+
+/* List all jobs */
+void list_jobs() {
+    for (int i = 0; i < job_count; i++) {
+        printf("[%d] %s (%s)\n", jobs[i].id, 
+            jobs[i].command, 
+            jobs[i].status == RUNNING ? "Running" : "Stopped");
+    }
+}
 
 /* print prompt and read a line */
 int userin(char *p)
 {
     int c, count;
-    
+
     /* initialization for alter routines */
     ptr = inpbuf;
     tok = tokbuf;
@@ -119,64 +201,122 @@ int procline(void) /* process input line */
                 if (narg < MAXARG)
                     narg++;
                 break;
-            case EOL:
-            case SEMICOLON:
-            case AMPERSAND:
-                if (toktype == AMPERSAND)
-                    type = BACKGROUND;
-                else
-                    type = FOREGROUND;
-                
-                if (narg != 0)
-                {
-                    arg[narg] = NULL;
-                    runcommand(arg, type);
-                }
 
-                if (toktype == EOL)
-                    return 0;
-                
-                narg = 0;
-                break;
+            case EOL:
+case SEMICOLON:
+case AMPERSAND:
+    if (toktype == AMPERSAND)
+        type = BACKGROUND;
+    else
+        type = FOREGROUND;
+
+    if (narg != 0)
+    {
+        arg[narg] = NULL;
+
+        // Built-in commands
+        if (strcmp(arg[0], "jobs") == 0) {
+            list_jobs();  // List jobs when "jobs" is entered
+        } else if (strcmp(arg[0], "bg") == 0) {
+            Job *job = find_job_by_id(atoi(arg[1]));
+            if (job) {
+                kill(-job->pgid, SIGCONT);  // Resume job in background
+                job->status = RUNNING;
+            } else {
+                printf("No such job\n");
+            }
+        } else if (strcmp(arg[0], "fg") == 0) {
+            Job *job = find_job_by_id(atoi(arg[1]));
+            if (job) {
+                foreground_pid = job->pgid;  // Move job to foreground
+                kill(-job->pgid, SIGCONT);   // Resume job
+                waitpid(-job->pgid, NULL, 0);  // Wait for it to complete
+                delete_job(job->pgid);  // Remove from jobs list
+            } else {
+                printf("No such job\n");
+            }
+        } else if (strcmp(arg[0], "kill") == 0) {
+            if (arg[1] == NULL) {
+                printf("Usage: kill <job_id>\n");
+            } else {
+                Job *job = find_job_by_id(atoi(arg[1]));  // Find job by ID
+                if (job) {
+                    // Send SIGKILL to the entire process group
+                    if (kill(-job->pgid, SIGKILL) == -1) {
+                        perror("kill");  // Print error if unable to send signal
+                    } else {
+                        printf("Job [%d] (%s) terminated.\n", job->id, job->command);
+                        delete_job(job->pgid);  // Remove the job from the list
+                    }
+                } else {
+                    printf("No such job\n");
+                }
+            }
+        } else {
+            runcommand(arg, type);  // Run other commands
+        }
+    }
+
+    if (toktype == EOL)
+        return 0;
+
+    narg = 0;
+    break;
         }
     }
 }
 
 /* execute a command with optional wait */
-int runcommand(char **cline, int where)
-{
+int runcommand(char **cline, int where) {
     pid_t pid;
     int status;
 
-    switch(pid = fork()){
+    switch (pid = fork()) {
         case -1:
             perror("smallsh");
             return (-1);
         case 0:
+            // Child process
+            setpgid(0, 0);  // Set the child process group to its own PID
+            signal(SIGINT, SIG_DFL);
+            signal(SIGTSTP, SIG_DFL);
             execvp(*cline, cline);
             perror(*cline);
             exit(1);
     }
 
-    /* code for parent */
-    /* if background process print pid and exit */
-    if (where == BACKGROUND)
-    {
+    // Parent process
+    if (where == BACKGROUND) {
+        setpgid(pid, pid);  // Ensure the child process group is set
         printf("[Process id %d]\n", pid);
+        add_job(pid, cline[0], RUNNING);  // Add background job
         return (0);
     }
 
-    /* wait until process pid exits */
-    if (waitpid(pid, &status, 0) == -1)
+    foreground_pid = pid;
+    setpgid(pid, pid);  // Ensure the foreground process group is set
+
+    if (waitpid(pid, &status, WUNTRACED) == -1) {
+        perror("waitpid");
         return (-1);
-    else   
-        return (status);
+    }
+
+    if (WIFSTOPPED(status)) {
+        printf("\n[Process %d stopped]\n", pid);
+        add_job(pid, cline[0], STOPPED);  // Add stopped job
+    }
+
+    foreground_pid = -1;
+    return status;
 }
 
 char *prompt = "Command> "; /* prompt */
 
 int main()
 {
+    // Set up SIGINT handler
+    signal(SIGINT, handle_sigtstp);
+
     while (userin(prompt) != EOF)
         procline();
 
