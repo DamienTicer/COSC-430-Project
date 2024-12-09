9acd12e3b127646ff4cdd661c9f617a5022b791d
Damien Ticer <125095430+DamienTicer@users.noreply.github.com>
2024-12-08 22:24:28 -0500

    Update smallsh.h
    Full functionality of project specifications

--- smallsh.h
+++ smallsh.h
@@ -13,3 +13,15 @@
 
 #define FOREGROUND 0
 #define BACKGROUND 1
+
+#define STOPPED 2  /* Job is stopped */
+#define RUNNING 3  /* Job is running */
+
+typedef struct {
+    int id;            /* Job ID */
+    pid_t pgid;        /* Process group ID */
+    char command[MAXBUF]; /* Command string */
+    int status;        /* RUNNING, STOPPED */
+} Job;
+
+#define MAXJOBS 20  /* Maximum number of jobs */
